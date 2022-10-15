3c6ada060f4de1bdc7d7b5673c796d055c6c94f9
rochmanramadhani12 <rochmanramadhani.dev@gmail.com>
2022-10-16 00:11:46 +0700

    feat: add seed for rack model

---
+++ internal/driver/db/seeder/seed/rack.go
@@ -0,0 +1,44 @@
+package seed
+
+import (
+	"github.com/sirupsen/logrus"
+	"gitlab.com/stoqu/stoqu-be/internal/model/entity"
+	"gitlab.com/stoqu/stoqu-be/pkg/constant"
+	"gitlab.com/stoqu/stoqu-be/pkg/util/str"
+	"gorm.io/gorm"
+)
+
+type RackSeed struct{}
+
+func (s *RackSeed) Run(conn *gorm.DB) error {
+	trx := conn.Begin()
+
+	rackNames := []string{"Rack 1", "Rack 2", "Rack 3", "Rack 4", "Rack 5"}
+	var racks []entity.RackModel
+	for _, v := range rackNames {
+		rack := entity.RackModel{
+			RackEntity: entity.RackEntity{
+				Code: str.GenCode(constant.CODE_RACK_PREFIX),
+				Name: v,
+			},
+		}
+		racks = append(racks, rack)
+	}
+
+	if err := trx.Create(&racks).Error; err != nil {
+		trx.Rollback()
+		logrus.Error(err)
+		return err
+	}
+
+	if err := trx.Commit().Error; err != nil {
+		logrus.Error(err)
+		return err
+	}
+
+	return nil
+}
+
+func (s *RackSeed) GetTag() string {
+	return `rack_seed`
+}
